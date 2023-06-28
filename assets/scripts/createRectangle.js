a686cc4844f66e1fa81d9eff72175210bf00b66a
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2023-06-28 15:34:32 +0200

    add navbar and front adjustement in CSS

--- assets/scripts/createRectangle.js
+++ assets/scripts/createRectangle.js
@@ -1,17 +1,45 @@
-  export function createRectangles(containerId, rectangleWidth, rectangleClass, adjustment = 0, horizontal = false) {
-      var container = document.getElementById(containerId);
-      var largeurEcran = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//   export function createRectangles(containerId, rectangleWidth, rectangleClass, adjustment = 0, horizontal = false) {
+//     var container = document.getElementById(containerId);
+//     var largeurEcran = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//     var largeurEcran = (document.width !== undefined) ? document.width : document.body.offsetWidth;
+//     if(horizontal){
+//         largeurEcran = Math.floor(largeurEcran - 2 * 133);
+//     }
 
-      if(horizontal){
-          largeurEcran = Math.floor(largeurEcran - 2 * 133);
-      }
+//     // var nombreRectangles = Math.floor(largeurEcran / rectangleWidth) + adjustment;
+//     // var nombreRectangles = Math.floor();
+//     var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+//     var _docWidth = (document.width !== undefined) ? document.width : document.body.offsetWidth;
 
-      var nombreRectangles = Math.floor(largeurEcran / rectangleWidth) + adjustment;
+//     var nombreRectangles = Math.floor(_docHeight / rectangleWidth) + adjustment;
+//     console.log('nb rectangle', nombreRectangles);
 
-      for (var i = 0; i < nombreRectangles; i++) {
-          var rectangle = document.createElement("div");
-          rectangle.classList.add(rectangleClass);
-          container.appendChild(rectangle);
-      }
+//     for (var i = 0; i < nombreRectangles; i++) {
+//         var rectangle = document.createElement("div");
+//         rectangle.classList.add(rectangleClass);
+//         container.appendChild(rectangle);
+//     }
+// }
+
+export function createRectangles(containerId, rectangleWidth, rectangleClass, adjustment = 0, horizontal = false) {
+  var container = document.getElementById(containerId);
+  var largeurEcran = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  // if(horizontal){
+  //     largeurEcran = Math.floor(largeurEcran - 2 * 133);
+  // }
+
+  // var nombreRectangles = Math.floor(largeurEcran / rectangleWidth) + adjustment;
+  // var nombreRectangles = Math.floor();
+  const _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+  const _docWidth = (document.width !== undefined) ? document.width : document.body.offsetWidth;
+
+  const nombreRectangles = Math.ceil(_docHeight / rectangleWidth) + adjustment;
+  console.log('nb rectangle', nombreRectangles);
+
+  for (var i = 0; i < nombreRectangles; i++) {
+      var rectangle = document.createElement("div");
+      rectangle.classList.add(rectangleClass);
+      container.appendChild(rectangle);
   }
-  
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,18 +6,31 @@ import { toggleTab } from "./changeTab.js";
 toggleTab()
 
 window.onload = function() {
-
-  // generateFilmsContentTab();
-
+  createRectangles("container1-big-rectangle-V", 121, "big-rectangle-v", 1);
   createRectangles("container1-small-rectangle-top-V", 14, "small-rectangle-v");
-  createRectangles("container1-big-rectangle-V", 121, "big-rectangle-v");
+  // createRectangles("container1-big-rectangle-V", 121, "big-rectangle-v", 1);
   createRectangles("container1-small-rectangle-bot-V", 14, "small-rectangle-v");
 
-  createRectangles("container-small-rectangle-top-H", 14, "small-rectangle-h", 0, true);
-  createRectangles("container-big-rectangle-H", 121, "big-rectangle-h", 0, true);
-  createRectangles("container-small-rectangle-bot-H", 14, "small-rectangle-h", 0, true);
-
+  createRectangles("container2-big-rectangle-V", 121, "big-rectangle-v", -1);
   createRectangles("container2-small-rectangle-top-V", 14, "small-rectangle-v");
-  createRectangles("container2-big-rectangle-V", 121, "big-rectangle-v");
   createRectangles("container2-small-rectangle-bot-V", 14, "small-rectangle-v");
+
+  // createRectangles("container-small-rectangle-top-H", 14, "small-rectangle-h", -5, true);
+  // createRectangles("container-big-rectangle-H", 121, "big-rectangle-h", 0, true);
+  // createRectangles("container-small-rectangle-bot-H", 14, "small-rectangle-h", -5, true);
+  // generateFilmsContentTab();
+
+
+
+  // createRectangles("container1-small-rectangle-top-V", 14, "small-rectangle-v");
+  // createRectangles("container1-big-rectangle-V", 121, "big-rectangle-v");
+  // createRectangles("container1-small-rectangle-bot-V", 14, "small-rectangle-v");
+
+  // createRectangles("container-small-rectangle-top-H", 14, "small-rectangle-h", 0, true);
+  // createRectangles("container-big-rectangle-H", 121, "big-rectangle-h", 0, true);
+  // createRectangles("container-small-rectangle-bot-H", 14, "small-rectangle-h", 0, true);
+
+  // createRectangles("container2-small-rectangle-top-V", 14, "small-rectangle-v");
+  // createRectangles("container2-big-rectangle-V", 121, "big-rectangle-v");
+  // createRectangles("container2-small-rectangle-bot-V", 14, "small-rectangle-v");
 };
