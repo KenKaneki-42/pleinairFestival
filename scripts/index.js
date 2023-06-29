c54508047dc6009a0ee519b4fd6e79cd2e95e8af
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2023-06-29 15:40:45 +0200

    Add lottie panda on fims's pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
 import { toggleTab } from "./changeTab.js";
 import { FooterComponent } from "./footerComponent.js";
 import { createRectangles } from './createRectangle.js';
-
+import { loadLottie } from "./lottieAnimation.js";
 let url = window.location.pathname;
 let filename = url.substring(url.lastIndexOf('/')+1);
+
 if(filename == "homepage.html") {
   toggleTab();
+}
+
+if (window.location.pathname.includes("/Pages/films/")) {
+  loadLottie();
 }
 
 customElements.define('footer-component', FooterComponent);
@@ -17,16 +22,9 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log(docHeight);
   createRectangles("container1-big-rectangle-V", 121, "big-rectangle-v", docHeight, -1);
   createRectangles("container2-big-rectangle-V", 121, "big-rectangle-v", docHeight, -1);
-
   createRectangles("container1-small-rectangle-top-V", 14, "small-rectangle-v", docHeight, 4);
   createRectangles("container1-small-rectangle-bot-V", 14, "small-rectangle-v", docHeight, 4);
   createRectangles("container2-small-rectangle-top-V", 14, "small-rectangle-v", docHeight, 4);
   createRectangles("container2-small-rectangle-bot-V", 14, "small-rectangle-v", docHeight, 4);
-  // createRectangles("container1-big-rectangle-V", 121, "big-rectangle-v", docHeight, 1);
-  // createRectangles("container1-small-rectangle-top-V", 14, "small-rectangle-v", docHeight, 7);
-  // createRectangles("container1-small-rectangle-bot-V", 14, "small-rectangle-v", docHeight, 7);
 
-  // createRectangles("container2-big-rectangle-V", 121, "big-rectangle-v", docHeight, 1);
-  // createRectangles("container2-small-rectangle-top-V", 14, "small-rectangle-v", docHeight, 7);
-  // createRectangles("container2-small-rectangle-bot-V", 14, "small-rectangle-v", docHeight, 7);
 });
